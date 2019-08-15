827ced367aff56fadf5b330aaf78e4d6594aab16
Valeryi Savich <relrin78@gmail.com>
2019-08-16 00:33:29 +0300

    Moved get_configs function to utils module

--- src/client.rs
+++ src/client.rs
@@ -6,7 +6,7 @@ use crate::terminal::{
     print_warning,
     print_error
 };
-use crate::utils::get_files_list;
+use crate::utils::get_configs;
 
 
 pub struct Client;
@@ -30,29 +30,13 @@ impl Client {
     }
 
     fn show_configurations(&self, directory: &String) -> Result<(), SageError> {
-        let configs = get_files_list(directory)?
-            .into_iter()
-            .filter(|f| {
-                match f.metadata() {
-                    Ok(metadata) => metadata.is_dir(),
-                    Err(_) => false
-                }
-            })
-            .filter(|f| f.file_name() == "configs")
-            .flat_map(|f| {
-                let dir = f.path().to_string_lossy().into_owned();
-                match get_files_list(&dir) {
-                    Ok(vec) => vec,
-                    Err(_) => vec![]
-                }
-            })
-            .map(|dir| dir.file_name().to_string_lossy().into_owned())
-            .collect::<Vec<String>>();
+        let configs = get_configs(directory)?;
 
         match configs.len() {
             count if count > 0 => {
                 print_info("Available configurations:");
                 configs
+                    .keys()
                     .into_iter()
                     .for_each(|config| print_info(&format!("- {}", config)));
             },
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
 use std::fs::{self, DirEntry};
 
 use quick_error::ResultExt;
 
 use crate::error::SageError;
+
+
+pub const CONFIG_DIRECTORY_NAME: &'static str = "configs";
 
 
 pub fn get_files_list(path: &String) -> Result<Vec<DirEntry>, SageError> {
@@ -11,6 +15,33 @@ pub fn get_files_list(path: &String) -> Result<Vec<DirEntry>, SageError> {
         .collect();
 
     Ok(files)
+}
+
+
+pub fn get_configs(path: &String) -> Result<HashMap<String, String>, SageError> {
+    let configs = get_files_list(path)?
+        .into_iter()
+        .filter(|f| {
+            match f.metadata() {
+                Ok(metadata) => metadata.is_dir(),
+                Err(_) => false
+            }
+        })
+        .filter(|f| f.file_name() == CONFIG_DIRECTORY_NAME)
+        .flat_map(|f| {
+            let dir = f.path().to_string_lossy().into_owned();
+            match get_files_list(&dir) {
+                Ok(vec) => vec,
+                Err(_) => vec![]
+            }
+        })
+        .map(|dir| (
+            dir.file_name().to_string_lossy().into_owned(), // directory name
+            dir.path().to_string_lossy().into_owned(),      // path
+        ))
+        .collect();
+
+    Ok(configs)
 }
 
 
