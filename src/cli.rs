2340fb4f1360781ff3e00c0bfd586ac24cb593ac
Valeryi Savich <Relrin78@gmail.com>
2019-08-16 18:50:41 +0300

    Added generate command to CLI

--- src/cli.rs
+++ src/cli.rs
@@ -14,5 +14,35 @@ pub enum Command {
             help="Path to directory with Terraform files"
         )]
         directory: String
+    },
+    #[structopt(name = "generate")]
+    /// Generate main.tf from the template file
+    Generate {
+        #[structopt(
+            required = true,
+            help="Configuration name"
+        )]
+        config: String,
+        #[structopt(
+            short = "d",
+            long = "dir",
+            default_value = ".",
+            help="Path to directory with Terraform files"
+        )]
+        directory: String,
+        #[structopt(
+            short = "t",
+            long = "target",
+            default_value = "main.tpl",
+            help="Path to the used template file (*.tpl)"
+        )]
+        target: String,
+        #[structopt(
+            short = "o",
+            long = "out",
+            default_value = "main.tf",
+            help="Path to the generated file"
+        )]
+        out: String
     }
 }
