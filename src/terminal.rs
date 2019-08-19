9f55d3d0a467847c54577a01944b4c3e91d19abc
Valeryi Savich <relrin78@gmail.com>
2019-08-19 23:10:57 +0300

    Added documentation for terminal module

--- src/terminal.rs
+++ src/terminal.rs
@@ -2,18 +2,22 @@ use ansi_term::Colour::{Green, Red, Yellow};
 
 use crate::error::SageError;
 
+// Prints message that the job's done.
 pub fn print_command_done() {
     println!("[{}] Done.", Green.paint("OK"))
 }
 
+// Prints regular message in terminal.
 pub fn print_info(message: &str) {
     println!("[{}] {}", Green.paint("INFO"), message)
 }
 
+// Prints warning message in terminal.
 pub fn print_warning(message: &str) {
     println!("[{}] {}", Yellow.paint("WARNING"), message)
 }
 
+// Prints error message in terminal.
 pub fn print_error(err: SageError) {
     println!("[{}] {}", Red.paint("ERROR"), err)
 }
